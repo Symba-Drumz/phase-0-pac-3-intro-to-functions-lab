8e9eb68befbb922ff86180fd3e898f42bb1d0e7e
alfazachmmosi <zalfa3890@gmail.com>
2024-03-18 19:41:34 +0300

    updated

--- test/indexTest.js
+++ test/indexTest.js
@@ -2,19 +2,19 @@ require ( './root.js' );
 
 
 describe('shout(string)', function() {
-  it('receives one argument and returns it in all caps', function() {
+  it('should receive one argument and return it in all caps', function() {
     expect(shout('hello')).toEqual('HELLO');
-  })
-})
+  });
+});
 
 describe('whisper(string)', function() {
-  it('receives one argument and returns it in all lowercase', function() {
+  it('should receive one argument and return it in all lowercase', function() {
     expect(whisper('HELLO')).toEqual('hello');
-  })
-})
+  });
+});
 
 describe('logShout(string)', function() {
-  it('takes a string argument and logs it in all caps using console.log()', function() {
+  it('should take a string argument and log it in all caps using console.log()', function() {
     const spy = expect.spyOn(console, 'log').andCallThrough();
 
     logShout('hello');
@@ -22,11 +22,11 @@ describe('logShout(string)', function() {
     expect(spy).toHaveBeenCalledWith('HELLO');
 
     console.log.restore();
-  })
-})
+  });
+});
 
 describe('logWhisper(string)', function() {
-  it('takes a string argument and logs it in all lowercase using console.log()', function() {
+  it('should take a string argument and log it in all lowercase using console.log()', function() {
     const spy = expect.spyOn(console, 'log').andCallThrough();
 
     logWhisper('HELLO');
@@ -34,21 +34,21 @@ describe('logWhisper(string)', function() {
     expect(spy).toHaveBeenCalledWith('hello');
 
     console.log.restore();
-  })
-})
+  });
+});
 
 describe('sayHiToHeadphonedRoommate(string)', function() {
-  it('returns "I can\'t hear you!" if `string` is lowercase', function() {
+  it('should return "I can\'t hear you!" if the input is lowercase', function() {
     expect(sayHiToHeadphonedRoommate('hello')).toEqual("I can't hear you!");
-  })
+  });
 
-  it('returns "YES INDEED!" if `string` is uppercase', function() {
+  it('should return "YES INDEED!" if the input is uppercase', function() {
     expect(sayHiToHeadphonedRoommate('HELLO')).toEqual("YES INDEED!");
-  })
+  });
 
-  it('returns "I would love to!" if `string` is "Let\'s have dinner together!"`', function () {
+  it('should return "I would love to!" if the input is "Let\'s have dinner together!"', function() {
     expect(sayHiToHeadphonedRoommate("Let's have dinner together!")).toEqual(
       "I would love to!"
     );
   });
-})
+});
--- test/root.js
+++ test/root.js
@@ -1,30 +1,35 @@
 global.expect = require('expect');
 
-const babel = require('babel-core');
+const babel = require('@babel/core');
 const jsdom = require('jsdom');
 const path = require('path');
 
 before(function(done) {
   const babelResult = babel.transformFileSync(
     path.resolve(__dirname, '..', 'index.js'), {
-      presets: ['es2015']
+      presets: ['@babel/preset-env']
     }
   );
 
-  const html = path.resolve(__dirname, '..', 'index.html')
+  const html = path.resolve(__dirname, '..', 'index.html');
 
-  jsdom.env(html, [], {
+  jsdom.env({
+    html,
+    scripts: [
+      'https://code.jquery.com/jquery-3.5.1.min.js'
+    ],
     src: babelResult.code,
-    virtualConsole: jsdom.createVirtualConsole().sendTo(console)
-  }, (err, window) => {
-    if (err) {
-      return done(err);
+    virtualConsole: jsdom.createVirtualConsole().sendTo(console),
+    done: (err, window) => {
+      if (err) {
+        return done(err);
+      }
+
+      Object.keys(window).forEach(key => {
+        global[key] = window[key];
+      });
+
+      return done();
     }
-
-    Object.keys(window).forEach(key => {
-      global[key] = window[key];
-    });
-
-    return done();
   });
 });
